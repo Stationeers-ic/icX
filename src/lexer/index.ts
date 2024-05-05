7aef02e1e0f0a91537f79b6f08059dd2182f767e
Aidan647 <kirillkotka@gmail.com>
2024-05-05 22:42:02 +0300

    Update index.ts

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,13 +1,8 @@
 import extractTextBetween from "./textBetween"
 import { TOKENS, TOKEN_TYPES } from "./tokens"
 
-const text = `const shuffle = array => {
-     for (let i = array.length - 1; i > 0; i--) {
-       const j = Math.floor(Math.random() * (i + 1));
-       [array[i], array[j]] = [array[j], array[i]];
-     }
-     return array;
-   }; `
+const text = `const shuffle = "hello" // h */
+`
 
 type Token = {
 	type: TOKEN_TYPES
